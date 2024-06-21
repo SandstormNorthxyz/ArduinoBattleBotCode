641d4d8beca86df4a65081e252bfefd6477a919f
YaLlama <47946874+YaLlama@users.noreply.github.com>
2024-06-20 20:44:32 -0400

    still works

---
+++ src/utilities/PID.h
@@ -0,0 +1,48 @@
+//
+// Created by Kaden Cassidy on 6/20/24.
+//
+
+#ifndef ARDUINOBATTLEBOTCODE_PID_H
+#define ARDUINOBATTLEBOTCODE_PID_H
+
+#include "ringBuffer.h"
+
+class PID{
+
+    public:
+        PID(double kP, double kI, double kD);
+
+        void setConstants(double kP, double kI, double kD);
+
+        void setFeedForward(double constant);
+
+        void setDeadZone(double deadZone);
+
+        void setHomedThreshold(double homedThreshold);
+
+        void setHomedConstant(double homedConstant);
+
+        void setLowerLimit(double constant);
+
+        void reset();
+
+        float getCorrection(double current, double target);
+    private:
+        double kP;
+        double kI;
+        double kD;
+        double feedForwardConstant;
+        double deadZone = deadZone;
+        double homedThreshold;
+        double homedConstant = homedConstant;
+        double lowerLimitConstant;
+        double integralSum;
+        ringBuffer<double, 3>* prevErrorRingBuffer;
+        ringBuffer<double, 3>* timeRingBuffer;
+
+        bool signum(double number){
+            return number <= 0;
+        }
+};
+
+#endif //ARDUINOBATTLEBOTCODE_PID_H
